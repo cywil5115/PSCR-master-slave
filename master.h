aae677e46da34013352a7015a72ac7ab049d90fb
cywil5115 <116916841+cywil5115@users.noreply.github.com>
2025-02-17 16:11:14 +0100

    edit headers (PROJECT DONE)

--- master.h
+++ master.h
@@ -5,16 +5,55 @@
 #define A32 0x02  //(1<<1)
 #define A64 0x04  //(1<<2)
 
-#define USER4 0x0B
+//AMODE (Address Mode): This field defines the VMEbus Address mode.
+#define M_A16 0x00
+#define M_A24 0x01  //(1<<0)
+#define M_A32 0x02  //(1<<1)
+#define M_A64 0x04  //(1<<2)
+#define M_CR_CSR 0x05
+#define M_USER1 0x08
+#define M_USER2 0x09
+#define M_USER3 0x0A
+#define M_USER4 0x0B
 
-#define NON_PRV_ACCESS (0<<4) // 0x00
-#define SUP_ACCESS (1<<4) // 0x10
+// ENABLE
+#define M_EN 0x80000000 //the corresponding outbound translation function is enabled
+#define M_NOT_EN 0x00000000
 
-#define PGM_PROGRAM (1<<3) // 0x08
-#define PGM_DATA 0x00
+//MRPFD (Memory Read Prefetch Disable)
+#define M_PREFETCH_DISABLED 0x40000
+#define M_PREFETCH 0x00 // a cache line is prefetched when a PCI/X bus memory read burst is received
 
-#define DBW16 0x00  //VMEbus Data Bus Width
-#define DBW32 0x40 // (1<<6) 6 bit od zera liczac
+//PFS (Prefetch Size)
+#define M_CACHE2LINES 0x00
+#define M_CACHE4LINES 0x10000
+#define M_CACHE8LINES 0x20000
+#define M_CACHE16LINES 0x30000
+
+//2eSST Transfer Rate
+#define M_MB160 0x00
+#define M_MB267 0x800
+#define M_MB320 0x1000
+
+//VMEbus Transfer Mode
+#define M_SCT 0x00
+#define M_BLT 0x100
+#define M_MBLT 0x200
+#define M_TWOeVME 0x300
+#define M_TWOeSST 0x400
+#define M_TWOeSST_BROADCAST 0x500 // 5<<8
+
+//VMEbus Supervisory Mode
+#define M_NON_PRV_ACCESS (0<<5) // 0x00
+#define M_SUP_ACCESS (1<<5) // 0x10
+
+//VMEbus Program Mode
+#define M_PGM_PROGRAM (1<<4) // 0x08
+#define M_PGM_DATA 0x00
+
+//VMEbus Data Bus Width (DBW)
+#define M_DBW16 0x00  //VMEbus Data Bus Width
+#define M_DBW32 0x40 // (1<<6) 6 bit od zera liczac
 
 
 uint32_t to_big_endian(uint32_t val);
@@ -41,13 +80,5 @@ void* set_master (
             uint32_t attribute_address
 
             );
-
-
-
-
-
-
-
-
 
 #endif /* MASTER_H */
--- slave.h
+++ slave.h
@@ -1,6 +1,72 @@
 #ifndef SLAVE_H
 #define SLAVE_H
 
+// ENABLE
+#define S_EN 0x80000000
+#define S_NOT_EN 0x00000000
+
+/* TH (Threshold): This field sets a threshold for when read-ahead prefetching resumes. If set,
+prefetching resumes once the FIFO is half empty. If cleared, prefetching resumes once the
+FIFO is completely empty.*/
+#define S_TH_ON 0x40000
+#define S_TH_OFF 0x00000
+
+// VFS (Virtual FIFO Size)
+#define S_FIFO64 0x00000
+#define S_FIFO128 0x10000
+#define S_FIFO256 0x20000
+#define S_FIFO512 0x30000
+
+//2eSSTM (2eSSTM)
+/*These bits define the 2eSST transfer rates the corresponding VME
+Slave responds to. If SST320 is enabled, the VME Slave also responds to SST267 and
+SST160. If SST267 is enabled, the VME Slave also responds to SST160.*/
+#define S_SST160 0x0000
+#define S_SST267 0x1000
+#define S_SST320 0x2000
+
+// 2eSSTB (2eSSTB): If set, the corresponding VME Slave responds to 2eSST broadcast cycles.
+#define S_TWOeSSTB_ON 0x800
+#define S_TWOeSSTB_OFF 0x000
+
+//2eSST (2eSST): If set, the corresponding VME Slave responds to standard 2eSST cycles.
+#define S_TWOeSST_ON 0x400
+#define S_TWOeSST_OFF 0x000
+
+//2eVME (2eVME): If set, the corresponding VME Slave responds to 2eVME cycles.
+#define S_TWOeVME_ON 0x200
+#define S_TWOeVME_OFF 0x000
+
+//MBLT (MBLT): If set, the corresponding VME Slave responds to MBLT cycles.
+#define S_MBLT_ON 0x100
+#define S_MBLT_OFF 0x000
+
+//BLT (BLT): If set, the corresponding VME Slave responds to BLT cycles.
+#define S_BLT_ON 0x80
+#define S_BLT_OFF 0x00
+
+//AS (Address Space): These bits define the address space the corresponding VME Slave responds to.
+#define S_A16 (0<<4)
+#define S_A24 0x10
+#define S_A32 0x20
+#define S_A64 0x40
+
+// SUPR (Supervisor): If set, the corresponding VME Slave is enabled to respond to VMEbus supervisor access cycles.
+#define S_SUPR_ON 0x08
+#define S_SUPR_OFF 0x00
+
+//NPRIV (Non-privileged): If set, the corresponding VME Slave is enabled to respond to non-privileged access cycles.
+#define S_NPRIV_ON 0x04
+#define S_NPRIV_OFF 0x00
+
+// PGM (Program): If set, the corresponding VME Slave is enabled to respond to VMEbus program access cycles.
+#define S_PGM_ON 0x02
+#define S_PGM_OFF 0x00
+
+// DATA (Data): If set, the corresponding VME Slave is enabled to respond to VMEbus data access cycles
+#define S_DATA_ON 0x01
+#define S_DATA_OFF 0x00
+
 uint32_t slave_to_big_endian(uint32_t val);
 void set_slave_starting_upper_address(volatile struct register_info *regs, uint32_t address, int slave_index);
 void set_slave_starting_lower_address(volatile struct register_info *regs, uint32_t address, int slave_index);
